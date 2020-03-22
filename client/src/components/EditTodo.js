2e26c84cd0aeb6d09614167a07b717a625ecbdf2
mitchavery <avery.mitch651@gmail.com>
2020-03-21 17:48:00 -0700

    added editing

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,18 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description);
+
+  const updateDescription = async e => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:5000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body)
+        }
+      );
+
+      window.location ="/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <Fragment>
       <button
         type="button"
         className="btn btn-warning"
         data-toggle="modal"
-        data-target="#myModal"
+        data-target={`#id${todo.todo_id}`}
       >
         Edit
       </button>
 
-      <div className="modal" id="myModal">
+      <div className="modal" id={`id${todo.todo_id}`}>
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
@@ -23,7 +44,12 @@ const EditTodo = ({ todo }) => {
             </div>
 
             <div className="modal-body">
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+              />
             </div>
 
             <div className="modal-footer">
@@ -31,6 +57,7 @@ const EditTodo = ({ todo }) => {
                 type="button"
                 className="btn btn-warning"
                 data-dismiss="modal"
+                onClick={e => updateDescription(e)}
               >
                 Edit
               </button>
